e5eef948cca11a1dc642d81afbae842094713de6
Rail <rail@ra1l-notebook.(none)>
2012-09-19 13:16:08 +0400

    creating

---
+++ upload.php
@@ -0,0 +1,70 @@
+<?php
+$uploadDir = 'img/'; //папка для хранения файлов
+$dir = ''; //базовый путь к скрипту
+$mwidth = 500;
+
+$allowedExt = array('jpg', 'jpeg', 'png', 'gif');
+$maxFileSize = 2 * 1024 * 1024; //2 MB
+
+//если получен файл
+if (isset($_FILES)) {
+    //проверяем размер и тип файла
+    $ext = end(explode('.', strtolower($_FILES['Filedata']['name'])));
+    if (!in_array($ext, $allowedExt)) {
+        return;
+    }
+    if ($maxFileSize < $_FILES['Filedata']['size']) {
+        return;
+    }
+    if (is_uploaded_file($_FILES['Filedata']['tmp_name'])) {
+	//Магия с созданием уникального имени. Начало
+	    $fileName = $uploadDir.$_FILES['Filedata']['name'];
+		$nameParts = explode('.', $_FILES['Filedata']['name']);
+        $nameParts[count($nameParts)-2] =substr(md5(time()),7);
+        $fileName = $uploadDir.implode('.', $nameParts);
+		//если файл с таким именем уже существует... Невероятное! =)
+        while(file_exists($fileName)) {
+            //...добавляем текущее время к имени файла
+            $nameParts = explode('.', $_FILES['Filedata']['name']);
+            $nameParts[count($nameParts)-2] =substr(md5(time()),7);
+            $fileName = $uploadDir.implode('.', $nameParts);
+        }
+		//Генерим путь к mini
+		$dirParts=explode('/',$fileName);
+		$dirParts[0].="/mini";
+		$fileName2 = implode('/',$dirParts);
+	//Магия с созданием уникального имени. Конец
+        move_uploaded_file($_FILES['Filedata']['tmp_name'], $fileName);
+		//Костыль ресайза картинок до width:500px; начало
+		// Get new sizes
+		list($width, $height, $type, $attr) = getimagesize($fileName);
+		if($width>500) {
+		$newwidth=500;
+		$k=$newwidth/$width;
+		$newheight = $height * $k;
+		// Load
+		$thumb = imagecreatetruecolor($newwidth, $newheight);
+		if($type==3) $source = imagecreatefrompng($fileName);
+		elseif($type==1) $source = imagecreatefromgif($fileName);
+		elseif($type==2) $source = imagecreatefromjpeg($fileName);
+		else die("IO error");
+	
+		// Resize
+		imagecopyresampled($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+		if($type==3) $source = imagepng($thumb, $fileName2);
+		elseif($type==1) $source = imagegif($thumb, $fileName2);
+		elseif($type==2) $source = imagejpeg($thumb, $fileName2);
+		else die("IO error");
+
+		$ext='<a href="'.$dir.$fileName.'"><img src="'.$dir.$fileName2.'"/></a>';
+		echo $ext."<br/>";
+		echo '<input type="text" size="150" value="'.htmlspecialchars($ext).'"><br />';
+		}
+		//Костыль ресайза картинок до width:500px; конец
+		else {
+        	$ext='<img src="'.$dir.$fileName.'"/>';
+		echo $ext."<br/>";
+		echo '<input type="text" size="150" value="'.htmlspecialchars($ext).'"><br />';
+		}
+    }
+}
